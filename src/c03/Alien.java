20c4cd2232a6e22aaed7cf674d4cf59b86274500
gfranco <gfranco@cpqi-for-0064.cpqi.com>
2014-03-18 17:09:22 -0300

    ++

--- src/c03/Alien.java
+++ src/c03/Alien.java
@@ -13,11 +13,14 @@ package c03;
 
 class Alien {
 	public static void main(String[] args) {
-		int[][] a = {{1,2,}, {3,4}};
+		int[][] myArray = new int[3][];
+		byte z=0;
+		z+=30;
+		int[][] a = {{1,2,},{3,4}};
 		int[] b = a[1];
 		Object o1 = a;
 		int[][] a2 = (int[][]) o1;
-		int[] b2 = (int[]) o1;
-		System.out.println(b[1]);
+		int[] b2 = (int[]) o1;// vai dar ClassCastException
+		System.out.println(z);
 	}
 }
