d609874e2660c936b6d49c6ebf008a4b146961c7
GleitonFranco <gleitonfranco@gmail.com>
2014-03-25 23:01:10 -0300

    news.

---
+++ src/c05/TestIf.java
@@ -0,0 +1,12 @@
+package c05;
+
+public class TestIf {
+
+	public static void main(String[] args) {
+		int x=5, y=2;
+		boolean t =true;
+		if ( ( (x<3)&&(y==2) ) | t )
+			System.out.println("True");
+	}
+
+}
